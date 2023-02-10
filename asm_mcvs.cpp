5ff82978162be0ee2b4e2df56371b839fce37a1b
Japrajah <70380700+Japrajah@users.noreply.github.com>
2023-02-10 13:01:30 +0700

    better example

--- asm_mcvs.cpp
+++ asm_mcvs.cpp
@@ -1,20 +1,22 @@
-﻿#include <iostream>
-#pragma region Shell
-int dummy()
-{
-    std::cout << "Hello World!\n";
-    return 1;
-}
+#include <iostream>
+__int64 dataptr = 0x00100023;
+int dummy() { std::cout << "Hello World!\n"; return 1;}
+int dummy2() {std::cout << "Hello dummy2!\n";  return 1;}
 
+
+#pragma region JmpToDataShell
 #pragma section(".text")
-__declspec(allocate(".text"))
-__int8 shelld[] = { 0x90 ,0x90 ,0x90, 0x90 , 0x49, 0x89, 0xC6 ,  0xC3 };
-__declspec(allocate(".text"))
-void* testptr[] = { &dummy };
-__declspec(allocate(".text"))
-__int8 shell[] = {  0xFF ,0x15 ,0x02, 0x00 ,0x00, 0x00, 0xEB, 0x08 }; //call 0xFF ,0x15 ,0x02, 0x00 ,0x00, 0x00, 0xEB, 0x08   
-#pragma endregion Shell
+__declspec(allocate(".text"))  void* DATA_jmp[3] = { (void*)0xA148909090909090,&dataptr  ,(void*)0xE0FFE0FFE0FFE0FF };
+// nop *6 ← 909090909090
+// mov rax, ← A148
+// [dataptr]   
+// jmp rax (x4) ←  E0FF
+#pragma endregion JmpToDataShell
+
 int main()
 {
-    reinterpret_cast<void(*)()>(&shell[0])(); // for test only
+    dataptr = (__int64)&dummy;
+    reinterpret_cast<void(*)()>(&DATA_jmp[0])();
+    dataptr = (__int64)&dummy2;
+    reinterpret_cast<void(*)()>(&DATA_jmp[0])();
 }
